bdd9415bec84df9c402de2d16767428d3bf84569
Lukas <lukas.kern2@gmail.com>
2018-04-16 17:02:59 +0200

    Test scene for coordinate system transformation

---
+++ Assets/Scripts/CoordTransformTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoordTransformTest : MonoBehaviour {
+
+    public CoordsysTransform coordTransform;
+
+    public Transform[] triangleFrom;
+    public Transform[] triangleTo;
+
+    // Use this for initialization
+    void Start () {
+		for (int i=0; i<3; i++)
+        {
+            coordTransform.SavePositionPair(triangleFrom[i].position, triangleTo[i].position);
+        }
+
+        coordTransform.CreateTransformation();
+
+        foreach (Transform t in triangleFrom)
+            t.position = coordTransform.ApplyTransformationTo(t.position);
+    }
+}
