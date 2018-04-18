7909bb4badf47e60c1dd9fda7a8dcc5e0f5bc4d1
Lukas <lukas.kern2@gmail.com>
2018-04-18 20:30:25 +0200

    Add visualization scene and scripts that it uses
    ObjectVisibility.cs listens to key combinations to toggle the visibility
    of the Optitrack and haptic GameObjects

---
+++ Assets/Scripts/ObjectVisibility.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectVisibility : MonoBehaviour {
+
+    public GameObject optiCursorLeft;
+    public GameObject optiCursorRight;
+    public GameObject unityTarget;
+
+    public GameObject hapticCursorLeft;
+    public GameObject hapticCursorRight;
+    public GameObject hapticTargetLeft;
+    public GameObject hapticTargetRight;
+
+    public GameObject inkwellLeft;
+    public GameObject inkwellRight;
+
+    // Update is called once per frame
+    void Update () {
+        //Optitrack (Unity)
+        if (Input.GetKey(KeyCode.O))
+        {
+            Debug.Log("O");
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Debug.Log("L");
+                optiCursorLeft.SetActive(!optiCursorLeft.activeSelf);
+            }
+            else if (Input.GetKeyDown(KeyCode.R))
+            {
+                optiCursorRight.SetActive(!optiCursorRight.activeSelf);
+            }
+            else if (Input.GetKeyDown(KeyCode.T))
+            {
+                unityTarget.SetActive(!unityTarget.activeSelf);
+            }
+        }
+        //Haptic
+        else if (Input.GetKey(KeyCode.H))
+        {
+            if (Input.GetKey(KeyCode.L))
+            {
+                if (Input.GetKeyDown(KeyCode.C))
+                {
+                    hapticCursorLeft.SetActive(!hapticCursorLeft.activeSelf);
+                }
+                else if (Input.GetKeyDown(KeyCode.T))
+                {
+                    hapticTargetLeft.SetActive(!hapticTargetLeft.activeSelf);
+                }
+            }
+            else if (Input.GetKey(KeyCode.R))
+            {
+                if (Input.GetKeyDown(KeyCode.C))
+                {
+                    hapticCursorRight.SetActive(!hapticCursorRight.activeSelf);
+                }
+                else if (Input.GetKeyDown(KeyCode.T))
+                {
+                    hapticTargetRight.SetActive(!hapticTargetRight.activeSelf);
+                }
+            }
+        }
+        //Inkwell
+        else if (Input.GetKey(KeyCode.I))
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                inkwellLeft.SetActive(!inkwellLeft.activeSelf);
+            }
+            else if (Input.GetKeyDown(KeyCode.R))
+            {
+                inkwellRight.SetActive(!inkwellRight.activeSelf);
+            }
+        }
+	}
+}
