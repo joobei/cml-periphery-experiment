4fb97ed531133cae32d5c50f885adb7e8aae8d2e
Lukas <lukas.kern2@gmail.com>
2018-04-24 18:46:08 +0200

    Improve object (visibility) toggler script

--- Assets/Scripts/ObjectVisibility.cs
+++ Assets/Scripts/ObjectVisibility.cs
@@ -9,27 +9,92 @@ public class ObjectVisibility : MonoBehaviour {
     public GameObject unityTarget;
 
     public GameObject hapticCursorLeft;
+    private MeshRenderer hCLMR;
+
     public GameObject hapticCursorRight;
+    private MeshRenderer hCRMR;
+
     public GameObject hapticTargetLeft;
+    private MeshRenderer hTLMR;
+
     public GameObject hapticTargetRight;
+    private MeshRenderer hTRMR;
 
     public GameObject inkwellLeft;
+    private MeshRenderer iLMR;
+
     public GameObject inkwellRight;
+    private MeshRenderer iRMR;
+
+
+    void Start()
+    {
+        hCLMR = hapticCursorLeft.GetComponent<MeshRenderer>();
+        hCRMR = hapticCursorRight.GetComponent<MeshRenderer>();
+        hTLMR = hapticTargetLeft.GetComponent<MeshRenderer>();
+        hTRMR = hapticTargetRight.GetComponent<MeshRenderer>();
+        iLMR = inkwellLeft.GetComponent<MeshRenderer>();
+        iRMR = inkwellRight.GetComponent<MeshRenderer>();
+    }
 
     void Update ()
     {
-        ToggleVisibility();
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            ToggleVisibilityHaptics();
+        }
+        else if (Input.GetKeyDown(KeyCode.I))
+        {
+            ToggleVisibilityInkwells();
+        }
+        //else if (Input.GetKeyDown(KeyCode.Backspace))
+        //{
+        //    ToggleObjectsAll();
+        //}
     }
 
-    public void ToggleVisibility()
+    //Toggle MeshRenderer component of haptic objects
+    public void ToggleVisibilityHaptics()
+    {
+        hCRMR.enabled = hCLMR.enabled;
+        hTLMR.enabled = hCLMR.enabled;
+        hTRMR.enabled = hCLMR.enabled;
+
+        hCLMR.enabled = !hCLMR.enabled;
+        hCRMR.enabled = !hCRMR.enabled;
+        hTLMR.enabled = !hTLMR.enabled;
+        hTRMR.enabled = !hTRMR.enabled;
+        Debug.Log("Turned haptic visibility " + hTRMR.enabled);
+    }
+
+    //Toggle MeshRenderer component of inkwells
+    public void ToggleVisibilityInkwells()
+    {
+        iLMR.enabled = iRMR.enabled;
+
+        iLMR.enabled = !iLMR.enabled;
+        iRMR.enabled = !iRMR.enabled;
+        Debug.Log("Turned inkwell visibility " + iLMR.enabled);
+    }
+
+    public void ToggleObjectsAll()
+    {
+        hapticCursorLeft.SetActive(!hapticCursorLeft.activeSelf);
+        hapticTargetLeft.SetActive(!hapticTargetLeft.activeSelf);
+        hapticCursorRight.SetActive(!hapticCursorRight.activeSelf);
+        hapticTargetRight.SetActive(!hapticTargetRight.activeSelf);
+        inkwellLeft.SetActive(!inkwellLeft.activeSelf);
+        inkwellRight.SetActive(!inkwellRight.activeSelf);
+    }
+
+    //Toggle gameobjects based on key combinations
+    public void ToggleObjects()
     {
         //Optitrack (Unity)
         if (Input.GetKey(KeyCode.O))
         {
-            Debug.Log("O");
             if (Input.GetKeyDown(KeyCode.L))
             {
-                Debug.Log("L");
                 optiCursorLeft.SetActive(!optiCursorLeft.activeSelf);
             }
             else if (Input.GetKeyDown(KeyCode.R))
