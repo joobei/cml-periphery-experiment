e10a6039717aee48f21d9c4e2a1bbbbe6e22c7a1
Lukas <lukas.kern2@gmail.com>
2018-04-24 18:57:32 +0200

    Add script to place Optitrack target in the middle
    between the Optitrack cursors

---
+++ Assets/Scripts/InitOptiTarget.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitOptiTarget: MonoBehaviour {
+
+    public Transform cursorOptiLeft;
+    public Transform cursorOptiRight;
+    public Transform optiTarget;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            optiTarget.position = cursorOptiRight.position + (cursorOptiLeft.position - cursorOptiRight.position) / 2;
+            optiTarget.GetComponent<MeshRenderer>().enabled = false;
+            Debug.Log("Placed Optitrack target between Optitrack cursors.");
+        }
+    }
+}
