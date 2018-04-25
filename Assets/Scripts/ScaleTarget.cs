bbb2c69a4adfc0c315db84aa332af638811179af
Lukas <lukas.kern2@gmail.com>
2018-04-25 20:37:28 +0200

    Add target scaling script (numpad +/-)
    Script for scaling up/down the Unity target and the haptic targets
    respectively

---
+++ Assets/Scripts/ScaleTarget.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleTarget : MonoBehaviour {
+
+    public GameObject[] transformations;
+
+    private GenericFunctionsClass myGenericFunctionsClassScript;
+
+    void Start()
+    {
+        myGenericFunctionsClassScript = transformations[0].GetComponent<ApplyTransformation>().myGenericFunctionsClassScript;
+    }
+
+    void Update () {
+		if (Input.GetKeyDown(KeyCode.KeypadPlus))
+        {
+            transform.localScale = transform.localScale + Vector3.one * 0.05f;
+            ScaleHapticTargets();
+        }
+        else if (Input.GetKeyDown(KeyCode.KeypadMinus))
+        {
+            transform.localScale = transform.localScale - Vector3.one * 0.05f;
+            ScaleHapticTargets();
+        }
+    }
+
+    private void ScaleHapticTargets()
+    {
+        foreach (GameObject gO in transformations)
+        {
+            float scaling = gO.GetComponent<CoordsysTransform>().scaling;
+            ApplyTransformation aT = gO.GetComponent<ApplyTransformation>();
+            aT.hapticTarget.localScale = transform.localScale * scaling;
+        }
+        myGenericFunctionsClassScript.UpdateHapticObjectMatrixTransform();
+    }
+}
