d9168ed657f06447d7019fa2b894a70912055cce
Lukas <lukas.kern2@gmail.com>
2018-06-18 15:23:17 +0200

    Add input manager script for mouse and keyboard
    presses. (Omni stylus buttons are checked in Touchy's HapticManager.cs)

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class InputManager : MonoBehaviour
+{
+    public delegate void MouseAction();
+    public static event MouseAction OnMouseButton;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (OnMouseButton != null)
+                OnMouseButton();
+        }
+    }
+}
