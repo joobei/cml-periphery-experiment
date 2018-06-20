844469885e53bc7267ef3495af6c0a07f8bcd0e7
Lukas <lukas.kern2@gmail.com>
2018-06-20 19:04:50 +0200

    Script for Optitrack target to keep scale of haptic target

---
+++ Assets/Scripts/Haptics/AdoptHapticScale.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdoptHapticScale : MonoBehaviour {
+
+    public HapticManager hapticManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        float radiusMeters = hapticManager.radius * 0.001f; //convert form mm to m
+        transform.localScale = Vector3.one * radiusMeters * 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = hapticManager.targetSphere.transform.localScale;
+    }
+}
