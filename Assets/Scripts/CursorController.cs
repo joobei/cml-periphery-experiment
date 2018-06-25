99737d5c96b9dee381490b8da12018a667f3f699
Lukas <lukas.kern2@gmail.com>
2018-06-25 14:00:38 +0200

    Backup cursor controller utility script that isn't used anymore

---
+++ Assets/Scripts/CursorController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime);
+        }else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime);
+        }
+	}
+}
